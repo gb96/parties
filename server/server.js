472a104ffb41c9d10c8d4a75d6d3fdde946c693e
greg <greg@embertec.com>
2015-03-24 15:53:55 +1030

    attempting to fix email config

--- server/server.js
+++ server/server.js
@@ -11,3 +11,9 @@ Meteor.publish("parties", function () {
   return Parties.find(
     {$or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]});
 });
+
+// Required to send email:
+Meteor.startup(function () {
+  process.env.MAIL_URL = "smtp://" + process.env.MAIL_USER + ":' + encodeURIComponent(process.env.MAIL_PASSWORD) + "@smtp.gmail.com:465";
+//  process.env.MAIL_URL = "smtp://username%40gmail.com:InsertYourGoogleAuthToken@smtp.gmail.com:465/";
+}
